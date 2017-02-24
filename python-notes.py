0ed46103a2bad233ba28b75a3c398e5a2711678a
Justin Huynh <j.alexhuynh@gmail.com>
2017-02-23 22:49:16 -0800

    Add comment annotations to python notes.

--- python-notes.py
+++ python-notes.py
@@ -7,30 +7,50 @@ Multi Line Comment
 """
 
 # ---------------------- Creating a variable
-var variableName = value
+variable_name = value 		# Written as lower_case.
+variable_types = ["Int", "Float", "String"]
 
 
 # ---------------------- Common Commands
-print variableName
-max(1, 2, 3, 4)
-min(1, 2, 3, 4)
-type(variableName)
+print variable_name			# Prints something to the console.
+max(1, 2, 3, 4)				# Finds the max value in a list.
+min(1, 2, 3, 4)				# Finds the min value in a list.
+abs(1)						# Finds the absolute value of a number.
+type(variable_name)			# Returns the type of the passed argument.
+
+from datetime import datetime
+now = datetime.now()
+	#now.year/month/day/hour/minute/second
+
+import math
+
+boolean_true = True 
+boolean_false = False
+	# Evaluated in this order:
+	# not > and > or
+
+variable_name = raw_input("Question")	# Promps the user to input a string response.
 
 
 # ---------------------- Common String Methods
-"string"[0] 
-len("string")
-str(101)
-"String".lower
-"String".upper
+"string"[0] 				# Accesses the letter at that index of the string.
+len("string")				# Returns the number of characters in a string.
+str(101)					# Turns a non-string into a string.
+"String".lower 				# Returns an all-lower-case string.
+"String".upper 				# Returns an all-upper-case string.
+"String".isalpha()			# Checks if a string is solely alphabet letters.
+string_var[1:5]				# Returns a substring from the first index, to before the second index.
+							# Can Use [0:len(string_var)] to encompass the beginning and end.
 
-"What\'s my name?"
+'What\'s my name?'			# Escapes the use of ' which can end a string early.
 
-print "Random text %s. Sentence number %d." % ("filler", 2)
+print "String " + "concatenation " + "is easy!"
+
+print "String %s. Integer %d. Float %f" % ("String", 1, 1.00)
 
 
 # ---------------------- Conditionals
-if x > y:
+if x > y:					# Will run code depending on if conditions are satisfied.
 	# code code
 elif x < y:
 	# code code
@@ -39,43 +59,51 @@ else:
 
 
 # ---------------------- Functions
-def functionName(argument):
+def function_name(argument):				# Call a function via function_name()
 	#code code
 
 
 # ---------------------- Modules
-import moduleName
-from moduleName import moduleMethod
-from moduleName import *
+import module_name							# Imports a module, such as math or datetime.
+from module_name import module_method		# Function import; imports a method from a module.
+from module_name import *					# Universal import; imports all the module's functions.
+											# But removes the need to type the module name.
 
 
 # ---------------------- Lists
-listName = [1, 2, 3, 4]
-listName[1:3]
-l
-istName.insert(0, "Zero")
-listName.sort()
-listName.remove(1)
-listName.pop(1)
+list_name = [1, 2, 3, 4]			# Creates a new list.
+list_name[1:3]						# Slices a list from first index, to before second index.
+		 [:3]						# Cuts from the beginning to the index.	
+		 [1:]						# Cuts from the index to the end.
+list_name.append("new_item")		# Adds an item to the end of the list.
+list_name.insert(0, "new_item")		# Inserts "new_item" at the specified index "0".
+list_name.sort()
+list_name.remove(1)					# Removes an item from a list.
+list_name.pop(1)					# Removes an item from the INDEX of a list.
 del(list[1])
 
 range(5)
 range(1,5)
 range(1,5,2)
 
-for item in listName:
+for item in list_name:				# Iterating over list items.
 	print item
 
-for i in range(len(listName)):
-	print list[i]
+for i in range(len(list_name)):		# Iterating over list indexes. 
+	print list[i]	
 
 
 # ---------------------- Dictionaries
-dictionaryName = {
+dictionary_name = {					# Creates a new dictionary.
 	'Key1' : 'Value1',
 	'Key2' : 'Value2',
 	'Key3' : [1, 2, 3, 4]
 }
 
-del dictionaryName[key]
-dictionaryName['Key3'][0]
+dictionary_name['new_key'] = 'new_value'	# Adds a new key to the dictionary.
+
+del dictionary_name[key]			# Deletes a key/value pair from the dictionary.
+dictionary_name['Key3'][0]			# Accesses an item within a list inside a dictionary.
+
+for key in dictionary_name:			# Loops over a dictionary.
+	print dictionary_name[key]	
